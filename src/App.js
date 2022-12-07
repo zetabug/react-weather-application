03d89596bc1457236e381aeb39529a13742dd74b
JPTomorrow <justin.parker.morrow@gmail.com>
2022-12-07 15:45:54 -0700

    I think I made it "look cool"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { useCallback } from 'react';
-import { useState, useEffect } from 'react';
+import React, { useCallback } from "react";
+import { useState, useEffect } from "react";
 
-import Weather from './components/Weather';
-import LoadingIndicator from './UI/LoadingIndicator';
+import Weather from "./components/Weather";
+import LoadingIndicator from "./UI/LoadingIndicator";
+import { BsFillGeoAltFill } from "react-icons/bs";
 
-import './app.css';
+import "./app.css";
 
 function App() {
   const [data, setData] = useState(null);
   const [place, setPlace] = useState(null);
-  const [search, setSearch] = useState('');
-  const [location, setLocation] = useState('');
+  const [search, setSearch] = useState("");
+  const [location, setLocation] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   function handleSearch(e) {
@@ -18,7 +19,7 @@ function App() {
   }
 
   async function geoHandler() {
-    setSearch('');
+    setSearch("");
     setIsLoading(true);
     await navigator.geolocation.getCurrentPosition((position) => {
       const crd = position.coords;
@@ -30,10 +31,10 @@ function App() {
 
   const fetchData = useCallback(async (search, location) => {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': '0173291af0msh62b3ca25953f210p13d732jsn66b4d9f97708',
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        "X-RapidAPI-Key": "0173291af0msh62b3ca25953f210p13d732jsn66b4d9f97708",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
       },
     };
     const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${
@@ -52,34 +53,30 @@ function App() {
     });
   }, [location, search, fetchData]);
 
-  let display = data ? (
-    <Weather place={place} data={data} />
-  ) : (
-    <p>no data found 😬</p>
-  );
-
   return (
     <>
       <div className="container">
-        <h1>WEATHER APPLICATION</h1>
-        <div className="search-sec">
-          <input
-            type="text"
-            value={search}
-            placeholder="Search by City..."
-            onChange={handleSearch}
-          />
+        <div className="widget-wrapper">
+          <div className="weather-header">
+            <h1>WEATHER</h1>
+            {isLoading && <LoadingIndicator />}
+          </div>
+          <div className="search-sec">
+            <input
+              type="text"
+              value={search}
+              placeholder="Search by City..."
+              onChange={handleSearch}
+              className="city-search"
+            />
+            <button onClick={geoHandler} className="find-me-btn">
+              <BsFillGeoAltFill size={18} />
+            </button>
+          </div>
+          <br />
+          <br />
+          {<Weather place={place} data={data} />}
         </div>
-        <div>
-          <p>or</p>
-        </div>
-        <div>
-          <button onClick={geoHandler}>Find me!</button>
-        </div>
-        <br />
-        <br />
-        {isLoading && <LoadingIndicator />}
-        {display}
       </div>
       <span className="credit">Ranvir@zetabug/github</span>
     </>
